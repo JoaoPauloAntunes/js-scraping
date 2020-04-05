dd351514de09870b1127c18a5379a59bb34ccfc9
João Paulo Antunes <48777730+JoaoPauloAntunes@users.noreply.github.com>
2020-04-05 01:57:27 -0300

    Add files via upload

---
+++ dirty-output.js
@@ -0,0 +1,27 @@
+// Here is the output data:
+[
+    {
+        "name": "  Tessera       ",
+        "description": " Proud of our wide range of product\n\t\t\t\twe developped many project in the past 4 years.  You can find the company \n\t\t\t\tin 14 different countries ",
+        "contact": {
+            "telephone": "       Phone: (841) 467-168  ",
+            "employee": {
+                "name": "  Mike Layn        ",
+                "jobTitle": "      Marketing Assistant",
+                "email": "      Email: mike.layn@tessera.io"
+            }
+        }
+    },
+    {
+        "name": "  Marcox      ",
+        "description": " Lorem ipsum dolor  ",
+        "contact": {
+            "telephone": "       Phone:    +1497 467168  ",
+            "employee": {
+                "name": "  Jake Kannegan      ",
+                "jobTitle": "   Owner ",
+                "email": "      Email:     jakek@marcox.com"
+            }
+        }
+    }
+]
---
+++ pretty-output.js
@@ -0,0 +1,31 @@
+// Here is the output data:
+{
+    "companies": [
+        {
+            "name": "Tessera",
+            "description": "Proud of our wide range of product we developped many project in the past 4 years. You can find the company in 14 different countries in the world. Blablabla.",
+            "url": "/comp/tessera",
+		"contact": {
+                "telephone": "841467168",
+                "employee": {
+                    "name": "Mike Layn",
+                    "jobTitle": "Marketing Assistant",
+                    "email": "mike.layn@tessera.io"
+                }
+            }
+        },
+        {
+            "name": "Marcox",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupid in any actions we take in the world.",
+            "url": "/comp/marcox",
+		"contact": {
+                "telephone": "1497467168",
+                "employee": {
+                    "name": "Jake Kannegan",
+                    "jobTitle": "Owner",
+                    "email": "jakek@marcox.com"
+                }
+            }
+        }
+    ]
+}
